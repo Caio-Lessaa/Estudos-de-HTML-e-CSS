464931706f3c1d0705fc6600481672877d12437f
Caio <caio.lessa01@fatec.sp.gov.br>
2025-09-10 16:33:22 -0300

    learning variables

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,3 +7,24 @@ console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos!");
 let eMaiorDeIdade = idade >= 18;
 
 console.log("É maior de idade? " + (eMaiorDeIdade ? "Sim" : "Não"));
+
+let endereco = null; 
+
+
+
+// let, const, var (tipos de variáveis em js)
+// const : não pode ser alterada o valor atribuido, e é necessário ser inicializada logo ao se declarar
+
+
+const pi = 3.14
+
+console.log(pi)
+
+try {
+    pi = 5;
+} catch (error) {
+    console.log("A variavel pi é uma constante, portanto não pode ser alterada!");
+}
+
+
+console.log(pi);
