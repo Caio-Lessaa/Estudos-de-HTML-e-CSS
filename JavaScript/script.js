e2a57e0f0faa065760fe97a98ad2b4d9480f47f4
Caio <caio.lessa01@fatec.sp.gov.br>
2025-09-10 14:56:20 -0300

    Learning fundamentals

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+let nome = "Caio";
+
+let idade = 21;
+
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos!");
+
+let eMaiorDeIdade = idade >= 18;
+
+console.log("É maior de idade? " + (eMaiorDeIdade ? "Sim" : "Não"));
